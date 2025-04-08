ad58ab60baeba1fa8957dc4c8c261d046185307c
adityagrover123 <adityagrover320@gmail.com>
2025-04-08 22:19:57 +0530

    this is chat working

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -29,6 +29,7 @@ export const getPosts = async (req, res) => {
 
 export const getPost = async (req, res) => {
   const id = req.params.id;
+
   try {
     const post = await prisma.post.findUnique({
       where: { id },
@@ -56,16 +57,23 @@ export const getPost = async (req, res) => {
               },
             },
           });
-          res.status(200).json({ ...post, isSaved: saved ? true : false });
+
+          return res.status(200).json({ ...post, isSaved: !!saved });
+        } else {
+          // If token is invalid, still respond once
+          return res.status(200).json({ ...post, isSaved: false });
         }
       });
+    } else {
+      // If no token, respond with isSaved: false
+      return res.status(200).json({ ...post, isSaved: false });
     }
-    res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
   }
 };
+
 
 export const addPost = async (req, res) => {
   const body = req.body;
